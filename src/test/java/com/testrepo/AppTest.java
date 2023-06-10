cab2445ecf7788bed39802d716ae095fc499cafa
André Silva <andre15andre@hotmail.com>
2023-06-10 17:06:10 +0200

    initial implementation

---
+++ src/test/java/com/testrepo/AppTest.java
@@ -0,0 +1,28 @@
+package com.testrepo;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    public AppTest(String testName)
+    {
+        super(testName);
+    }
+
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    public void testAppMath()
+    {
+        assertTrue(true);
+        //assertEquals(new AppMath().sum(1, 2), 3);
+    }
+}
